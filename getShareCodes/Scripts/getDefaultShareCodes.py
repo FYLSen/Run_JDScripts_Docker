696f1c768260349c81b72de3dbc303097d3716a3
Rhysn <cedaren@gmail.com>
2021-04-19 20:02:16 +0800

    bug fixes

--- getShareCodes/Scripts/getDefaultShareCodes.py
+++ getShareCodes/Scripts/getDefaultShareCodes.py
@@ -22,7 +22,7 @@ def main():
     setting = getSetting(preferences_path)
     shareCodes = ''
     for item in setting:
-        if(item.status == "dead" or item.status == "daily") continue
+        if item.status == "dead" or item.status == "daily" ：continue
         sc_group = []
         for CN in env.CONTAINER_NAME:
             logs_path = "/logs/%s/%s" % (CN, item.filename)
