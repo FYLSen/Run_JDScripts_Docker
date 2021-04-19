5dc370c08f787ade6899cfca0f820c7889191d10
Rhysn <cedaren@gmail.com>
2021-04-19 22:46:25 +0800

    .

--- updateShareCodes.sh
+++ updateShareCodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#curl -s -S -L ${SHARECODES_URL} > /Run_JDScripts_Docker/ShareCodes/Default.sh
+#curl -s -S -L --retry 33 ${SHARECODES_URL} > /Run_JDScripts_Docker/ShareCodes/Default.sh
 if [ ! -d "/ShareCodes" ]; then
   mkdir /ShareCodes
 fi
-curl -s -S -L ${SHARECODES_TEMP_URL} > /ShareCodes/Temp.sh
+curl -s -S -L --retry 33 ${SHARECODES_TEMP_URL} > /ShareCodes/Temp.sh
