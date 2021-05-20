fcd25ba3de6cbc3c36e5947cbaa1cb034686b46c
Rhysn <cedaren@gmail.com>
2021-05-21 00:09:29 +0800

    新活动

--- crontab.sh
+++ crontab.sh
@@ -26,6 +26,7 @@
 14 10,16 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/jd_try.js > /logs/jd_try.log 2>&1 | sed 's/^/ 【京东试用】/'; "
 15 8,13,18 17-31 5 * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/adolf_pk.js > /logs/adolf_pk.log 2>&1 | sed 's/^/ 【京享值PK】/'; "
 25 9 * 5,6 * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/adolf_oneplus.js > /logs/adolf_oneplus.log 2>&1 | sed 's/^/ 【一加手机】/'; "
+25 9 21-27 5 * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/adolf_mi.js > /logs/adolf_mi.log 2>&1 | sed 's/^/ 【Redmi合成小金刚】/'; "
 37 */2 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/monk_pasture.js > /logs/monk_pasture.log 2>&1 | sed 's/^/ 【有机牧场】/'; "
 20 9 20-31 5 * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/adolf_martin.js > /logs/adolf_martin.log 2>&1 | sed 's/^/ 【人头马X博朗】/'; "
 15 9 * 5,6 * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/adolf_flp.js > /logs/adolf_flp.log 2>&1 | sed 's/^/ 【飞利浦电视成长】/'; "
