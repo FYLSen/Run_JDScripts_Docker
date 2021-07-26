bead55a24dfdd282caa2e8c2ac91f0354d6f6c5f
Rhysn <cedaren@gmail.com>
2021-07-26 14:03:02 +0800

    .

--- getCookies/app/jdCookies/cookies.py
+++ getCookies/app/jdCookies/cookies.py
@@ -3,7 +3,9 @@
 from pickle import TRUE
 from PIL import Image
 import qrcode, requests, base64, time, re, os, json
+requests.packages.urllib3.disable_warnings()
 from urllib.parse import urlencode, quote_plus
+
 
 jd_ua = 'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'
 
