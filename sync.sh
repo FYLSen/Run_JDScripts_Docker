da2ca935c74b080a1c7f4aa8f3472eac9c3bc309
Rhysn <cedaren@gmail.com>
2021-10-18 23:24:04 +0800

    .

--- sync.sh
+++ sync.sh
@@ -9,6 +9,7 @@ git checkout .
 git pull
 
 npm install || npm install --registry=https://registry.npm.taobao.org || exit 1
+npm install -g typescript ts-node pm2 || exit 1
 
 crontab /Run_JDScripts_Docker/crontab.sh
 crontab -l
