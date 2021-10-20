1539968650e5edd1a685cddc90f13591eac2e62c
Rhysn <cedaren@gmail.com>
2021-10-20 18:41:00 +0800

    rm

--- crontab.sh
+++ crontab.sh
@@ -125,7 +125,7 @@
 7 0,17 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_bean_sign.ts > /logs/jd_bean_sign.log 2>&1 | sed 's/^/ 【京东多合一签到】/'; "
 0 9,12 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_bean_box.ts > /logs/jd_bean_box.log 2>&1 | sed 's/^/ 【京东双签】/'; "
 10 * * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_cfd.ts > /logs/jd_cfdts.log 2>&1 | sed 's/^/ 【京喜财富岛】/'; "
-10 * * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_cfd_hb.ts > /logs/jd_cfd_hb.log 2>&1 | sed 's/^/ 【京喜财富岛互幫】/'; "
+#10 * * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_cfd_hb.ts > /logs/jd_cfd_hb.log 2>&1 | sed 's/^/ 【京喜财富岛互幫】/'; "
 0 * * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_cfd_withdraw.ts >> /scripts/logs/jd_cfd_withdraw.log 2>&1 | sed 's/^/ 【京喜财富岛提现3】/'; "
 10 0 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_cfd_shell.ts > /logs/jd_cfd_shell.log 2>&1 | sed 's/^/ 【京喜财富岛提现2】/'; "
 10 0 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; ts-node /scripts/jd_cfd_balloon.ts > /logs/jd_cfd_balloon.log 2>&1 | sed 's/^/ 【京喜财富岛提现1】/'; "
