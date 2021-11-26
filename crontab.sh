12363bddd74c7fbcdf0734f624712fe2a5434c02
Rhysn <cedaren@gmail.com>
2021-11-26 18:38:41 +0800

    .

--- crontab.sh
+++ crontab.sh
@@ -67,6 +67,7 @@
 2,44 0,5,7 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall;  source /cookies; node /scripts/jd_speed_redpocke.js > /logs/jd_speed_redpocke.js.log 2>&1 | sed 's/^/ 【京东极速版红包】/'; "
 17,26 7 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/jd_speed_sign.js > /logs/jd_speed_sign.js.log 2>&1 | sed 's/^/ 【京东极速版签到】/'; "
 3 * * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /cookies; node /scripts/jd_super_redrain.js > /logs/jd_super_redrain.js.log 2>&1 | sed 's/^/ 【整点红包雨】/'; "
+0,1,30,31 * * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /cookies; node /scripts/jd_live_redrain.js > /logs/jd_live_redrain.js.log 2>&1 | sed 's/^/ 【整点红包雨】/'; "
 #30 10,12 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /cookies; node /scripts/jd_superBrand.js > /logs/jd_superBrand.js.log 2>&1 | sed 's/^/ 【首页下拉】/'; "
 4 1-23/4 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/jd_superMarket.js > /logs/jd_superMarket.js.log 2>&1 | sed 's/^/ 【东东超市】/'; "
 7,37 13,21 * * * bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/jd_dt.js > /logs/jd_dt.js.log 2>&1 | sed 's/^/ 【京东答题】/'; "
